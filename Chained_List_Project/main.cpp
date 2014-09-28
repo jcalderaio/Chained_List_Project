745a7771e90b65da7c16f842e54fd7ac14c39c94
John Calderaio <jcalderaio@gmail.com>
2014-09-28 12:29:43 -0400

    Initial Commit

---
+++ Chained_List_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chained_List_Project
+//
+//  Created by John Calderaio on 9/28/14.
+//  Copyright (c) 2014 John Calderaio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
